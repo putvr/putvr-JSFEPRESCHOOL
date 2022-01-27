18ba259ca63008b2e054464eac622f90a64a16ee
Roman Putilov <Putilov.Roman@gmail.com>
2022-01-27 13:04:38 +0500

    feat: save user config

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -16,6 +16,12 @@ list.addEventListener('click', ({target}) => {
 });
 
 /* Exercitium III */
+const config = {
+  theme: 'dark', 
+  lang: 'en',
+};
+
+const state =  {};
 
 const portfolioImages = document.querySelectorAll('.portfolio-img');
 const portfolioBtns = document.querySelector('.portfolio-btns-list');
@@ -46,56 +52,101 @@ preloadImages();
 
 // i18n
 import i18Obj from './translate.js';
-function getTranslate(lang) {
-  const elems = document.querySelectorAll('[data-i18]');
-  elems.forEach(e => {
-    e.textContent = i18Obj[lang][e.dataset.i18];
-    if (e.placeholder) {
-      e.textContent = '';
-      e.placeholder = i18Obj[lang][e.dataset.i18];
-    }
-  })
-}
 
 const langSwither = document.querySelector('.lang');
-let langSwitherActive = document.querySelector('.lang-active');
+langSwither.addEventListener('click', handleLangSwitch);
 
-langSwither.addEventListener('click', ({ target }) => {
+function handleLangSwitch ({ target }) {
   if(!target.dataset.lang) {
     return;
   }
-  langSwitherActive.classList.toggle('lang-active');  
-  getTranslate(target.dataset.lang);
-  langSwitherActive = target;
-  langSwitherActive.classList.toggle('lang-active');  
+  if(target.dataset.lang === state.lang) {
+    return;
+  }
+
+  state.langSwitherActive.classList.toggle('lang-active'); 
+  state.langSwitherActive = target;
+
+
+  setTranslate(target.dataset.lang); 
+  target.classList.toggle('lang-active'); 
+};
+
+
+function setTranslate(switchToLang) {
+  const elems = document.querySelectorAll('[data-i18]');
+
+  config.lang = switchToLang;
+  elems.forEach(e => {
+    e.textContent = i18Obj[config.lang][e.dataset.i18];
+    if (e.placeholder) {
+      e.textContent = '';
+      e.placeholder = i18Obj[config.lang][e.dataset.i18];
+    }
+  });
+
+  localStorage.setItem('lang', config.lang);
+}
+
+
+
+// theme
+const themeSwither = document.querySelector('.theme');
+themeSwither.addEventListener('click', ({ target }) => {
+  const swichToTheme = (config.theme === 'light') ? 'dark' : 'light';
+  setTheme(swichToTheme);  
 });
 
+function setTheme(newTheme) {  
+  document.body.classList.toggle(`${config.theme}-theme`);
 
+  config.theme = newTheme;
+  document.body.classList.toggle(`${config.theme}-theme`);
+
+  localStorage.setItem('theme', newTheme);
+}
+
+// local storage
+function getConfig() {  
+  let theme = localStorage.getItem('theme') ?? config.theme;
+  setTheme(theme);
+
+  let lang = localStorage.getItem('lang');  
+  state.langSwitherActive = document.querySelector(`.lang-active`);
+
+  if(lang || lang !== config.lang) {
+    const z = document.querySelector(`.lang__${lang}`);
+    z.click();
+  }
+  console.log(config, state);
+}
+
+window.addEventListener('load', getConfig);
 
 /* gradus  */ 
-console.log(`
+// console.log(`
 
-1) Вёрстка соответствует макету. Ширина экрана 768px +48
-  блок <header> +6
-  секция hero +6
-  секция skills +6
-  секция portfolio +6
-  секция video +6
-  секция price +6
-  секция contacts +6
-  блок <footer> +6
+// 1) Вёрстка соответствует макету. Ширина экрана 768px +48
+//   блок <header> +6
+//   секция hero +6
+//   секция skills +6
+//   секция portfolio +6
+//   секция video +6
+//   секция price +6
+//   секция contacts +6
+//   блок <footer> +6
 
-2) Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15
-  нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5
-  нет полосы прокрутки при ширине страницы от 768рх до 480рх +5
-  нет полосы прокрутки при ширине страницы от 480рх до 320рх +5
+// 2) Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15
+//   нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5
+//   нет полосы прокрутки при ширине страницы от 768рх до 480рх +5
+//   нет полосы прокрутки при ширине страницы от 480рх до 320рх +5
 
-3) На ширине экрана 768рх и меньше реализовано адаптивное меню +22
-  при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2
-  при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4
-  высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4
-  при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4
-  бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2
-  ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2
-  при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4
-`);
+// 3) На ширине экрана 768рх и меньше реализовано адаптивное меню +22
+//   при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2
+//   при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4
+//   высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4
+//   при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4
+//   бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2
+//   ссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +2
+//   при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4
+// `);
